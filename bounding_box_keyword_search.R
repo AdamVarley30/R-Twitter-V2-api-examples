e5b651cf3e1c737404f330b018aff367e7604bbc
AdamVarley30 <37413304+AdamVarley30@users.noreply.github.com>
2021-06-10 11:59:02 +0100

    Update bounding_box_keyword_search.R

--- bounding_box_keyword_search.R
+++ bounding_box_keyword_search.R
@@ -26,16 +26,35 @@ bearer_token <- "your-token"
 
 headers <- c(`Authorization` = sprintf('Bearer %s', bearer_token))
 
+i=2
 # Function for pulling correct datetime from Twitter response
 send_Mongo <- function(data,con){
-  for(i in 1:length(data)){
-    if(!is.null(data[[i]]$created_at)){
-      data[[i]]$created_at <- as.POSIXct(data[[i]]$created_at,format="%Y-%m-%dT%H:%M:%OS")
+  if(!is.null(data)){
+    # for list
+    for(i in 1:length(data)){
+      tryCatch(expr = {if(!is.null(data[[i]]$created_at)){
+        data[[i]]$created_at <- as.POSIXct(data[[i]]$created_at,format="%Y-%m-%dT%H:%M:%OS")
+        data[[i]]$ingestion_date <- Sys.time()
+        item <- data[[i]]
+        con$insert(item)
+      }}, error = function (err) {}
+      )
+      # for df  
+      tryCatch(expr = {if(!is.null(data[i,]$created_at)){
+        row = data[i,]
+        row$created_at <- as.POSIXct(row$created_at,format="%Y-%m-%dT%H:%M:%OS")
+        
+        row$ingestion_date <- Sys.time()
+        con$insert(row)
+      }}, error = function (err) {}
+      )
+      
+      returned <- 1
     }
-    data[[i]]$ingestion_date <- Sys.time()
-    item <- data[[i]]
-    con$insert(item)
+  } else {
+    returned <- 0
   }
+  return(returned)
 }
 
 # Main function
@@ -55,7 +74,7 @@ send_token_retrieve_data <- function(query_term_combined,tweets_per_request,star
   )
   # Next token will allow for pagination, kill if no token returned
   if(next_token != 0){params = c(params,next_token = next_token)}
-  message('querying Twitter')
+  # message('querying Twitter')
   
   # Search the correct endpoint - must have authentification for this - please see Twitter developer guide 
   response <- httr::GET(url = 'https://api.twitter.com/2/tweets/search/all', httr::add_headers(.headers=headers), query = params)
@@ -65,7 +84,7 @@ send_token_retrieve_data <- function(query_term_combined,tweets_per_request,star
   
   # Pull next token
   next_token <- list_tweets$meta$next_token
-  tweet_tot <- list_tweets$meta$result_count
+  tweet_tot <- ifelse(is.null(list_tweets$meta$result_count),0, list_tweets$meta$result_count)
   
   # insert into MongoDB tables the 4 separate tree responses from Twitter
   DBtweets  <- mongo(collection = 'tweets',db = db_name,url = conn)
@@ -74,28 +93,38 @@ send_token_retrieve_data <- function(query_term_combined,tweets_per_request,star
   DBtweetsInfo <- mongo(collection = 'tweets_info',db = db_name,url = conn)
   
   tryCatch(expr = {
-    send_Mongo(list_tweets$data,con = DBtweets)
-  },error = function(x) {
+    send_Mongo(data = list_tweets$data,con = DBtweets)
+  },error = function(err) {
+    message(err)
   })
   
   tryCatch(expr = {
-    send_Mongo(list_tweets$includes$users,con = DBusers)
-  },error = function(x) {
-    # message('No user information')
+    send_Mongo(data = list_tweets$includes$users,con = DBusers)
+  },error = function(err) {
+    message(err)
   })
   
-  tryCatch(expr = {    
-    data_list <- split(list_tweets$includes$places, seq(nrow(list_tweets$includes$places)))
-    send_Mongo(data = data_list,con = DBplaces)
-  },error = function(x) {
-  })
+  tryCatch(expr = {
+    if(!is.null(list_tweets$includes$places)){
+      data_list <- split(list_tweets$includes$places, seq(nrow(list_tweets$includes$places)))
+      send_Mongo(data = data_list,con = DBplaces)}
+  },error = function(err) {
+    message(err)
+  }
+  )
   
   tryCatch(expr = {
     send_Mongo(list_tweets$includes$tweets,con = DBtweetsInfo)
-  },error = function(x) {
+  },error = function(err) {
+    message(err)
   })
   
-  Sys.sleep(3)
+  if(!is.null(list_tweets$data)){
+    Sys.sleep(((tweet_tot/300)*2)+1)
+  } else {
+    Sys.sleep(0.5)
+  }
+  
   return_frame <- list(next_token = next_token, tweets_total = tweet_tot)
   return(return_frame)
 }
@@ -106,34 +135,38 @@ keyword_spatial_search <- function(db_name,start_date,end_date,spatial_query,key
   dropouts <- character()
   start_date <- as.POSIXct(start_date,format="%Y-%m-%dT%H:%M:%OS")
   end_date <- as.POSIXct(end_date,format="%Y-%m-%dT%H:%M:%OS")
-  # I have changed this to spatial searches there should be relatively few matches with the bbox
   date_sequence <- seq(start_date,end_date,by = 'year')
-
+  
   query_term_combined = paste(keyword_query,spatial_query)
   time_frame <- data.frame(from = as.character(date_sequence[-length(date_sequence[-1])],format="%Y-%m-%dT%H:%M:%OSZ"),to = as.character(date_sequence[-1],format="%Y-%m-%dT%H:%M:%OSZ"))
   i = 1
+  tweets_per_run <- 0
   for(i in nrow(time_frame):1){
     message('collecting tweets for : ', time_frame$from[i])
     tweets_tot <- 0
     next_token <- 0
     while(!is.null(next_token)){
-      tryCatch(expr = {      
+      tryCatch(expr = {
         list_data <- send_token_retrieve_data(query_term_combined,
-        tweets_per_request,
-        time_frame$from[i],time_frame$to[i], 
-        next_token,headers)
-      next_token <- list_data$next_token
-      tweets_tot <- tweets_tot + list_data$tweets_tot}
-      ,error = function(err) {
-        message(time_frame$from[i] ,'dropped out',err)
-        dropouts <- c(as.character(time_frame$from[i]),dropouts)
-      })
+                                              tweets_per_request,start_date_request = time_frame$from[i],end_date_request = time_frame$to[i], 
+                                              next_token,headers)
+        next_token <- list_data$next_token
+        tweets_tot <- tweets_tot + list_data$tweets_tot
+        
+        }
+        ,error = function(err) {
+          message(time_frame$from[i] ,' dropped out \n')
+          message(err)
+          
+          dropouts <- c(as.character(time_frame$from[i]),dropouts)
+        })
+      tweets_per_run <- tweets_per_run + tweets_tot
       
     }
     message('collected : ', tweets_tot, ' tweets')
   }
   
-  return(dropouts)
+  return(list(count = tweets_per_run, dropouts = dropouts))
 }
 
 ##################################################
@@ -158,17 +191,23 @@ st_bbox_by_feature = function(x) {
 # Turn into sf
 search_grid <- st_bbox_by_feature(search_grid) %>% st_as_sf()
 
-# Plot coordinates to check if they are right
+# Plot coordinates to check
 plot(search_grid,col = 'grey')
 
-# Query 
-keyword_query = '(football goal) lang:en'
-i = 1
+keyword_query = '(tremor OR quake OR shaking OR shook OR wobble OR seism OR quiver OR tremble OR shudder OR sway OR rock OR shudder OR "earth move" OR tectonic) lang:en'
+
+plot_progress <- search_grid
+plot_progress$counts <- NA
+
 for(i in 1:nrow(search_grid)){
   ext <- extent(search_grid[i,'x'])
+  
   # format bounding box 
   spatial_query <- paste0('bounding_box:[',paste(ext[1],ext[3],ext[2],ext[4],collapse = ' '),']')
- 
-      # call main function and give a new Mongo output DB. 
-      data_returned <- keyword_spatial_search(db_name = 'earthquakes',start_date = '2006-04-01T00:00:00Z',end_date = '2021-05-09T00:00:00Z', spatial_query = spatial_query , keyword_query = keyword_query,tweets_per_request = 100)
+  
+  # call main function and give a new Mongo output DB. 
+  data_returned <- keyword_spatial_search(db_name = 'earthquakes',start_date = '2006-04-01T00:00:00Z',end_date = '2021-05-09T00:00:00Z', spatial_query = spatial_query , keyword_query = keyword_query,tweets_per_request = 100)
+  plot_progress$counts[i] <- data_returned$count
+  # Plot map with updates of tweets collected
+  plot(plot_progress['counts'])
 }
